ed60d840e852cb45d8aa3b94500017531b99979c
florent69 <florent.ta@le-campus-numerique.fr>
2019-04-24 13:42:21 +0200

    3-objects finished

--- 3-objects/script.js
+++ 3-objects/script.js
@@ -11,7 +11,22 @@ const bus = {
     },
     equipments: ['sae', 'tft', 'speech']
 }
-
+// let keys = []
+// let values = []
+//
+// keys.push(Object.keys(bus))
+// values.push(Object.values(bus))
+//
+// for (let i = 0 ; i<keys.length ; i++) {
+//     console.log(keys[i] + ' ' + values[i])
+// }
+for (var i in bus) {
+    console.log(i + ' ' + bus[i])
+}
 //3.2 Object.values
-
 console.log(data)
+let text = []
+for (var iData in data) {
+    text.push(Object.values(data[iData].texte).join(''))
+console.log(iData + ' ' + text )
+}
